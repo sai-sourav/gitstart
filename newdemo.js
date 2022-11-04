cf6211622e975613a698c82ea48286cae2d525c5
sai-sourav <saisourav123@gmail.com>
2022-11-04 14:24:38 +0530

    Task 9 of DOM manipulations

--- newdemo.js
+++ newdemo.js
@@ -1,10 +1,13 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
 
 // Add item
@@ -13,14 +16,15 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
-
+  var newItem1 = document.getElementById('item2').value;
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
-
+  li.appendChild(document.createTextNode(' '));
+  li.appendChild(document.createTextNode(newItem1));
   ///
 
   // Create del button element
@@ -57,6 +61,7 @@ function addItem(e){
 
   // clear input field
   document.getElementById('item').value = '';
+  document.getElementById('item2').value = '';
 }
 
 // Remove item
@@ -67,4 +72,22 @@ function removeItem(e){
       itemList.removeChild(li);
     }
   }
+}
+
+// Filter Items
+function filterItems(e){
+  e.preventDefault();
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.innerText;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
