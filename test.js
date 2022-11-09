b2a85e34f39ab9d607885a4c90537f6c1a113136
sai-sourav <saisourav123@gmail.com>
2022-11-09 14:10:43 +0530

    call back functions

---
+++ test.js
@@ -0,0 +1,7 @@
+let button1=document.getElementById("but1");
+button1.addEventListener("click", function xyz(){
+    console.log("buton clicked")
+});
+document.addEventListener("DOMContentLoaded",()=>{
+    console.log("DOM has loaded");
+});
