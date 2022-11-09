5a3b59b4eccbab465c60c844def0c5b18e6be0d4
sai-sourav <saisourav123@gmail.com>
2022-11-09 14:10:16 +0530

    arrow vs normal functions

--- fatarrowVSnormalfunc.js
+++ fatarrowVSnormalfunc.js
@@ -19,6 +19,7 @@ function xyz() {
 }
 
 console.log(xyz.call(x)); // undefined
+console.log(xyz.call(this)); // undefined
 
 function xyz1() {
     abc1 = () => {
@@ -28,3 +29,18 @@ function xyz1() {
 }
 
 console.log(xyz1.call(x)); // local
+console.log(xyz1.call(this)); // global
+
+
+function xyz2() {
+    abc2= ()=>{
+        abc1 = () => {
+            return this.a;
+        }
+        return abc1();
+    }
+    return abc2();
+}
+
+console.log(xyz2.call(this)); //global
+console.log(xyz2.call(x)); // local
